f3097c3f02e792d5cf9ef2a7a2ce435f3d783c77
Fabian Kerzmann <fabinio@hotmail.de>
2021-01-12 13:08:13 +0100

    Implement multiple receivers for telegram bot messages

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const gpu_checker = require('./services/gpu_check')
 //Information needed by the telegram bot
 const telegramBotInfo = {
     accessToken: '1454487318:AAET-9jSJxV-qmYx7PD6ondtiZg7HPkjwsU',
-    chatId: '415412975'
+    chatId: 
+        {
+            fabian: '415412975',
+            tobias: '387131928'
+        }
 }
 
-
-sendTelegramMessage('Starting to check for GPUs')
+sendTelegramMessage('fabian', 'Starting to check for GPUs')
+sendTelegramMessage('tobias', 'Starting to check for GPUs')
 
 //set interval
 setInterval(function() {
@@ -21,9 +25,9 @@ setInterval(function() {
 
 }, 3600000)
 
-function sendTelegramMessage(message) {
+function sendTelegramMessage(receiver, message) {
     message = encodeURIComponent(message.trim())
-    request(`https://api.telegram.org/bot${telegramBotInfo.accessToken}/sendMessage?chat_id=${telegramBotInfo.chatId}&text=${message}`, { json: true }, (err, res, body) => {
+    request(`https://api.telegram.org/bot${telegramBotInfo.accessToken}/sendMessage?chat_id=${telegramBotInfo.chatId[$receiver]}&text=${message}`, { json: true }, (err, res, body) => {
         if (err) { return console.error(err) }
         console.log(body)
     })
--- services/gpu_check.js
+++ services/gpu_check.js
@@ -6,12 +6,28 @@ function checkAvailability() {
     request('https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=de-de', { json: true }, (err, res, data) => {
         if (err) { return console.error(err) }
 
+        data3080 = data.searchedProducts.productDetails.filter(item => {
+            return item.gpu === 'RTX 3080'
+        })
         data3070 = data.searchedProducts.productDetails.filter(item => {
             return item.gpu === 'RTX 3070'
         })
         data3060 = data.searchedProducts.productDetails.filter(item => {
             return item.gpu === 'RTX 3060 Ti'
         })
+
+        //check for RTX 3080
+        if (data3080.length > 0) {
+            const status = data3080[0].prdStatus
+            console.info('Availability (RTX 3080): ' + status)
+
+            if (status !== 'out_of_stock') {
+                sendTelegramMessage('tobias', 'RTX 3080 ist wieder verfügbar!')
+            }
+
+        } else {
+            console.info('The GPU was not found')
+        }
         
         //check for RTX 3070
         if (data3070.length > 0) {
@@ -19,7 +35,7 @@ function checkAvailability() {
             console.info('Availability (RTX 3070): ' + status)
 
             if (status !== 'out_of_stock') {
-                sendTelegramMessage('RTX 3070 ist wieder verfügbar!')
+                sendTelegramMessage('fabian', 'RTX 3070 ist wieder verfügbar!')
             }
 
         } else {
@@ -32,7 +48,7 @@ function checkAvailability() {
             console.info('Availability (RTX 3060 Ti): ' + status)
 
             if (status !== 'out_of_stock') {
-                sendTelegramMessage('RTX 3060 Ti ist wieder verfügbar!')
+                sendTelegramMessage('fabian', 'RTX 3060 Ti ist wieder verfügbar!')
             }
 
         } else {
